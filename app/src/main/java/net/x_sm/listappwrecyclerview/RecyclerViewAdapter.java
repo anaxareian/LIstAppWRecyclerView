d9923b28358dfcaa7d8b4c272c25acc5b5fad607
anaxareian <anaxareian@yahoo.com>
2019-01-13 16:39:49 -0500

    A ListApp that was built with the currently recommended Objects as the ListView is deprecated.  Based on a tutorial by youtube channel CodingWithMitch.  Good for experience building design on the text side, too.

---
+++ app/src/main/java/net/x_sm/listappwrecyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,83 @@
+package net.x_sm.listappwrecyclerview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import net.x_sm.listappwrecylcerview.R;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private Context mContext;
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mImageNames = new ArrayList<>();
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mImages, ArrayList<String> mImageNames) {
+        this.mContext = mContext;
+        this.mImages = mImages;
+        this.mImageNames = mImageNames;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Log.d(TAG, "onBindViewHolder: called for position " + Integer.toString(i));
+        final int position = i;
+
+        Glide.with(mContext).asBitmap().load(mImages.get(i)).into(viewHolder.image);
+
+        viewHolder.imageName.setText(mImageNames.get(i));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+
+                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
